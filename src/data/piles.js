1bc8ce84f2801fe8a3b2874456e7fd9233a72089
Pujitha <muttinenipujitha@gmail.com>
2026-05-05 15:09:28 +0530

    initual commit

---
+++ src/data/piles.js
@@ -0,0 +1,9 @@
+export const pilesData = {
+  treatment: "Piles",
+  usps: [
+    "No pain procedure",
+    "15-minute daycare treatment",
+    "Quick recovery",
+    "Experienced surgeons"
+  ]
+};
